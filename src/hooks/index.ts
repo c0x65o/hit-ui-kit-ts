1b04ecbd222406d97598d0f2d563cfa749142dc0
Clinton Mills <clinton.mills@spartancyberservices.com>
2025-12-31 08:44:28 -0600

    feat: update version and add hooks module in UI Kit

---
+++ src/hooks/index.ts
@@ -0,0 +1,20 @@
+/**
+ * UI Kit Hooks
+ * 
+ * This module exports all UI Kit hooks for targeted imports.
+ * Use `import { useAlertDialog } from '@hit/ui-kit/hooks'` for smaller bundles.
+ */
+
+export { useAlertDialog } from './useAlertDialog';
+export type { AlertDialogOptions, AlertDialogState } from './useAlertDialog';
+
+export { useTableView } from './useTableView';
+export type { TableView, TableViewFilter, TableViewShare } from './useTableView';
+
+export { useFormSubmit, parseError, parseResponseError } from './useFormSubmit';
+export type {
+  ParsedFormError,
+  FormSubmitState,
+  FormSubmitActions,
+  UseFormSubmitOptions,
+} from './useFormSubmit';
